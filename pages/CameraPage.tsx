6e2e2a5e111acdbcb2b5929dba586189e563ff2e
Hiroyuki1313 <munozdiaza@gmail.com>
2024-06-18 09:44:41 -0600

    kevin gei

--- pages/CameraPage.tsx
+++ pages/CameraPage.tsx
@@ -1,11 +1,14 @@
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import { CameraType } from 'expo-camera/build/legacy/Camera.types';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 
 export default function CameraPage() {
   const [facing, setFacing] = useState('back' as any);
   const [permission, requestPermission] = useCameraPermissions();
+  const [photo, SetPhoto] = useState();
+  const cameraRef = useRef(null as any)
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -21,6 +24,18 @@ export default function CameraPage() {
       </View>
     );
   }
+  if(photo){
+    return <Image style={{width:300, height:600}} source={photo.uri}/>
+  }
+
+  function takePhoto(){
+    if(cameraRef.current){
+        cameraRef.current.takePictureAsync({
+            // base64:true,
+            skipProcessing:true
+        }).then((PhotoData:any)=>{SetPhoto(PhotoData)})
+    } 
+    }
 
   function toggleCameraFacing() {
     setFacing((current:any) => (current === 'back' ? 'front' : 'back'));
@@ -28,16 +43,21 @@ export default function CameraPage() {
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePhoto}>
+            <Text style={styles.text}>Take Picture</Text>
           </TouchableOpacity>
         </View>
       </CameraView>
     </View>
   );
 }
+
+    
 
 const styles = StyleSheet.create({
   container: {
